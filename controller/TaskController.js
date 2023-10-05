18d0b3cb7442b7c851c683de5905e1c8db172d6a
Guilherme Vargas <110068068+VargasGui@users.noreply.github.com>
2023-10-05 16:49:58 -0300

    getbyid, update and delete v0.1

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -3,26 +3,75 @@ const Task = require("../models/Task.js")
 const getAll = async (req, res) => {
     try {
         const tasksList = await Task.find()
-        return res.render("index", {tasksList});
+        return res.render("index", { tasksList, taskId: null, taskDelete: null});
     } catch (error) {
-        res.status(500).send({error: error.message})
+        res.status(500).send({ error: error.message })
     }
 }
 
-const createTask = async (req,res) => {
+const createTask = async (req, res) => {
     const task = req.body
-    if(!task.task){
+    if (!task.task) {
         return res.redirect("/")
     }
     try {
         await Task.create(task)
         return res.redirect("/")
     } catch (error) {
-        res.status(500).send({error: error.message})
+        res.status(500).send({ error: error.message })
     }
 }
+
+const getById = async (req, res) => {
+    try {
+        const tasksList = await Task.find()
+        if(req.params.method == "update"){
+            const taskId = await Task.findOne({ _id: req.params.id })
+            res.render("index", { taskId, tasksList, taskDelete: null})
+        }else{
+            const taskDelete = await Task.findOne({ _id: req.params.id })
+            res.render("index", { taskId: null, tasksList, taskDelete})
+        }
+    } catch (error) {
+        res.status(500).send({ error: error.message })
+    }
+}
+
+const updateTask = async (req, res) =>{
+    try {
+        const task = req.body
+        await Task.updateOne({_id: req.params.id}, task)
+        res.redirect("/")
+    } catch (error) {
+        res.status(500).send({error: message})        
+    }
+}
+
+const confirmDelete = async (req,res) => {
+    try {
+        await Task.deleteOne({_id: req.params.id})
+        res.redirect("/")
+    } catch (error) {
+        res.status(500).send({error: message})
+    }
+}
+
+
+
+
+
+
+
+
+
 
 module.exports = {
     getAll,
     createTask,
+    getById,
+    updateTask,
+    confirmDelete
+
+
+
 }
--- routes/routes.js
+++ routes/routes.js
@@ -3,5 +3,8 @@ const TaskController = require("../controller/TaskController")
 
 routes.get("/", TaskController.getAll)
 routes.post("/create", TaskController.createTask)
+routes.get("/getById/:id/:method", TaskController.getById)
+routes.post("/updateOne/:id", TaskController.updateTask)
+routes.get("/confirmDelete/:id", TaskController.confirmDelete)
 
 module.exports = routes

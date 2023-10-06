8f59ef46d65bf747afbf84b889a4e7315425891c
Guilherme Vargas <110068068+VargasGui@users.noreply.github.com>
2023-10-06 13:49:05 -0300

    styling v0.1

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -3,7 +3,7 @@ const Task = require("../models/Task.js")
 const getAll = async (req, res) => {
     try {
         const tasksList = await Task.find()
-        return res.render("index", { tasksList, taskId: null, taskDelete: null});
+        return res.render("index", { tasksList, taskId: null, taskDelete: null });
     } catch (error) {
         res.status(500).send({ error: error.message })
     }
@@ -25,45 +25,36 @@ const createTask = async (req, res) => {
 const getById = async (req, res) => {
     try {
         const tasksList = await Task.find()
-        if(req.params.method == "update"){
+        if (req.params.method == "update") {
             const taskId = await Task.findOne({ _id: req.params.id })
-            res.render("index", { taskId, tasksList, taskDelete: null})
-        }else{
+            res.render("index", { taskId, tasksList, taskDelete: null })
+        } else {
             const taskDelete = await Task.findOne({ _id: req.params.id })
-            res.render("index", { taskId: null, tasksList, taskDelete})
+            res.render("index", { taskId: null, tasksList, taskDelete })
         }
     } catch (error) {
         res.status(500).send({ error: error.message })
     }
 }
 
-const updateTask = async (req, res) =>{
+const updateTask = async (req, res) => {
     try {
         const task = req.body
-        await Task.updateOne({_id: req.params.id}, task)
+        await Task.updateOne({ _id: req.params.id }, task)
         res.redirect("/")
     } catch (error) {
-        res.status(500).send({error: message})        
+        res.status(500).send({ error: message })
     }
 }
 
-const confirmDelete = async (req,res) => {
+const confirmDelete = async (req, res) => {
     try {
-        await Task.deleteOne({_id: req.params.id})
+        await Task.deleteOne({ _id: req.params.id })
         res.redirect("/")
     } catch (error) {
-        res.status(500).send({error: message})
+        res.status(500).send({ error: message })
     }
 }
-
-
-
-
-
-
-
-
-
 
 module.exports = {
     getAll,
@@ -71,7 +62,4 @@ module.exports = {
     getById,
     updateTask,
     confirmDelete
-
-
-
 }
